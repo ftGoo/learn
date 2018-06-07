ef7cddfc6a5f5475fa40d70f2892eb6f74e3471d
wb86518 <ytyhhhff@163.com>
2018-06-08 00:01:15 +0800

    完善后的鸡尾酒排序

--- arithmetic/src/com/wb/arithmetic/sort/bubblesort/cocktailSort.java
+++ arithmetic/src/com/wb/arithmetic/sort/bubblesort/cocktailSort.java
@@ -32,28 +32,87 @@ public class cocktailSort {
      */
 
     //第一次自己的尝试实现
-    //根据打印的结果来看，应该是正确的
+    //根据打印的结果来看，明显错了
+    //错误分析：右向左移动的时候出错。
     public static int[] cocktailSort(int[] array){
 
-        //由于是双向所以只需要进行array.length/2次,实际上还可以加上一个判断（在一轮没有交换的时候提前结束排序，时间问题不写了）
+        //由于是双向所以只需要进行array.length/2次,实际上还可以加上一个判断
+        //（在一轮没有交换的时候提前结束排序，时间问题不写了）
         for(int i = 0; i < array.length / 2; i++){
-            int index;
-            for(index = i; index <array.length- i -1 ; index++){
-                if(array[index] > array[index + 1]){
-                    int temp = array[index];
-                    array[index] = array[index + 1];
-                    array[index + 1] = temp;
+            int index = 0;
+            for(int j = 0 ; j <array.length- i -1 ; j++){
+                if(array[j] > array[j + 1]){
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
                 }
+                index = j;
             }
-
+            /**
+             * 以下注释大错特错，留下来提醒自己
+             */
             //由于for循环中的index++已经执行完，index已经比正处于的值大1，
             // 再减去index位已经确定该位置的数字不需要再比较再向左挪动1位再减1
             //如果把index写在for循环里面，for(j){index = j}也不需要这么绕
-            for(index = index - 2; index > i ; index--){
+            /*for(index = index - 1; index > i ; index--){
                 if(array[index] < array[index -1]){
                     int temp = array[index];
                     array[index] = array[index -1];
                     array[index -1] = temp;
+                }
+            }*/
+
+            //不是这个错误
+            /**
+            for(int j = index - 1; j > i ; j--){
+             if(array[j] < array[j -1]){
+             int temp = array[j];
+             array[j] = array[j -1];
+             array[j -1] = temp;
+             }
+             }
+             */
+            //错误原因已定位，index不可以-1（实际上index本来就只到arrany.length - 2的地方，坑啊）
+            System.out.println("没毛病吧");
+            System.out.println("index为" + index);
+            for(int j = index; j > i ; j--){
+                if(array[j] < array[j -1]){
+                    int temp = array[j];
+                    array[j] = array[j -1];
+                    array[j -1] = temp;
+                }
+            }
+
+            //继续探索
+
+
+            System.out.println("轮数" + i);
+            System.out.println(Arrays.toString(array));
+        }
+        return array;
+    }
+
+    /**
+     * 对上一个自己琢磨的稍微优化
+     */
+    public static int[] cocktailSort2(int[] array){
+
+        for(int i = 0; i < array.length / 2; i++){
+            int index = 0;
+            for(int j = i; j <array.length- i -1 ; j++){
+                if(array[j] > array[j + 1]){
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+                index = j;
+            }
+
+            for(int j = index - 1; j > i ; j--){
+                if(array[j] < array[j -1]){
+                    int temp = array[j];
+                    array[j] = array[j -1];
+                    array[j -1] = temp;
                 }
             }
 
@@ -64,7 +123,8 @@ public class cocktailSort {
     }
 
     /**
-     * 网上参考别人的实现后自己写的一个方法，好像比自己琢磨的优雅很多
+     * 网上参考别人的实现后的改进，好像比自己独立琢磨的优雅很多
+     * 果然要多看
      */
     public static int[] cocktailSortByOthers(int[] array){
         int row, up;
@@ -103,7 +163,7 @@ public class cocktailSort {
     public static void main(String[] args) {
         int []array = {23,22,13,11,5,88,14,24,6,100,25,11,20,15,75,3};
         System.out.println("没问题吧？");
-        cocktailSortByOthers(array);
+        cocktailSort(array);
     }
 
 }
